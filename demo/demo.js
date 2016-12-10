ccfbb5a176ae4f8f310616355f50b0cec0a26f49
munchkinhalfling <alex@rutins.com>
2016-12-10 17:31:59 -0500

    make the demo more demo-y

--- demo/demo.js
+++ demo/demo.js
@@ -1,3 +1,4 @@
 ((game) => {
-  // game code here
+  // game code here (In this case, add a sprite)
+  game.addSprite('newText', 'TextSprite', false, 'This is some normal text.');
 })(new Game('DemoGame'));
