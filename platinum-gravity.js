60f75d9a571f0a8e6b3f200d88f1e118f5ed5320
munchkinhalfling <alex@rutins.com>
2017-01-27 16:36:46 -0500

    actually create the gravity manager
    I like syntax highlighting :-)

--- platinum-gravity.js
+++ platinum-gravity.js
@@ -1,1 +1,29 @@
-
+var Platinum = Platinum || {};
+Platinum.GravityManager = class {
+  constructor(game, gravLevel) {
+    this._game = game;
+    this._timer = null;
+    this.yv = {};
+    for(let sprite of game.sprites) {
+      this.yv[sprite.name] = -(gravLevel);
+    };
+  }
+  startGravity() {
+    var game = this._game;
+    this._timer = setInterval(() => {
+      for(let sprite of game.sprites) {
+        if(sprite.touchable) {
+          if((sprite.getY() + 124) < WindowManager.getWindowDimensions().height && !sprite.detectCollisionRect()) {
+            sprite.setY(sprite.getY() + -(this.yv[sprite.name]));
+            this.yv[sprite.name] -= .5;
+          } else {
+            this.yv[sprite.name] = 0;
+          };
+        };
+      };
+    }, 100);
+  }
+  stopGravity() {
+    clearInterval(this._timer);
+  }
+};
