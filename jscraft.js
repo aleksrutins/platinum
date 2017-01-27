7f114b2c8ed5dcf56da540721a31407bd3bc163d
munchkinhalfling <alex@rutins.com>
2017-01-27 16:39:24 -0500

    add jscraft demo

---
+++ jscraft.js
@@ -0,0 +1,46 @@
+((game) => {
+  game.onReady(function() {
+    var win = document.getElementById('placeholderWin')
+    var hintsBox = document.getElementById('hints');
+    var domCounter = document.getElementById('counter');
+    var bi = 0;
+    var yv = {};
+    var timer;
+    var gravity = () => {
+      for(let sprite of game.sprites) {
+        if(sprite.touchable) {
+          if((sprite.getY() + 124) < WindowManager.getWindowDimensions().height && !sprite.detectCollisionRect()) {
+            sprite.setY(sprite.getY() + -(yv[sprite.name]));
+            yv[sprite.name] -= .5;
+          } else {
+            yv[sprite.name] = 0;
+          }
+        }
+      }
+    };
+    timer = setInterval(() => {
+      gravity();
+    }, 100);
+    win.style.zIndex = '-1';
+    game.getWin().addEventListener('click', e => {
+      var x = e.offsetX;
+      var y = e.offsetY;
+      var curBlock = game.addSprite('block' + bi, 'ImageSprite', true, 'block.svg', '[block]', 100, 100);
+      domCounter.innerHTML = (24 - bi).toString();
+      yv[curBlock.name] = -2;
+      curBlock.setPos(x - 50, y - 50);
+      //curBlock._el.style.zIndex = bi;
+      hintsBox.style.zIndex = (bi + 1).toString();
+      bi++;
+      if(bi == 25 || bi > 25) {
+        clearInterval(timer);
+        //game.clear();
+        var ohhb = game.addSprite('100blocks', 'ImageSprite', true, 'win.svg', 'You have placed 25 blocks!', 1000, 500);
+        ohhb._el.style.zIndex = 26;
+        ohhb._el.style.float = 'right';
+        ohhb.setClick(function(spr) {location.reload()});
+        hintsBox.style.display = 'none';
+      };
+    });
+  });
+})(new Game('JSCraft', {pointerLock: false, isFullScreen: true}));
