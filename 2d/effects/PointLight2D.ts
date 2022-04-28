3379ba788f31af968b2250907d9b8fc95afb66c3
Aleks Rutins <aleks@rutins.com>
2022-04-28 10:50:26 -0400

    The math still doesn't work...

--- 2d/effects/PointLight2D.ts
+++ 2d/effects/PointLight2D.ts
@@ -3,23 +3,28 @@ import { PostRenderEffect, RenderSystem2D } from "../RenderSystem2D.ts";
 import { Transform2D } from "../Transform2D.ts";
 
 export class PointLight2D implements PostRenderEffect {
-    #parabola(vx: number, vy: number): (x: number) => number {
-        return x => -(this.blurFactor * (x - vx)^2) + vy;
+    #parabola(x: number): number {
+        // Find the "slope" of the parabola, based on the x-intercepts (this.radius and -this.radius) and vertex ((0, this.brightness) - also the y-intercept, thankfully)
+        const xi1 = this.radius;
+        const xi2 = -this.radius;
+        const yi = this.brightness;
+        const a = (1 / (((-xi1 * x^-1) - (xi2 * x^-1) + (xi1 * xi2 * x^-2)) / yi));
+        // Return the parabola in vertex form
+        return (a * x) + yi;
     }
     #pointBrightness(x: number, y: number): number {
         const dist = Math.sqrt((x - this.cx)^2 + (y - this.cy)^2);
-        const f = this.#parabola(0, this.brightness);
-        return f(dist);
+        return this.#parabola(dist);
     }
     #pixelProp = (i: number) => i % 4
-    constructor(public cx: number, public cy: number, private screenWidth: number, private screenHeight: number, private brightness: number = 255, private blurFactor: number = 5) { }
+    constructor(public cx: number, public cy: number, private screenWidth: number, private screenHeight: number, private brightness: number = 255, private radius: number = 50) { }
     update(system: RenderSystem2D): void {
         const ctx = system.ctx;
         const image = ctx.getImageData(0, 0, this.screenWidth, this.screenHeight);
         image.data.forEach((value, index) => {
             const prop = this.#pixelProp(index);
-            const x = Math.floor(index / 4) % this.screenWidth;
-            const y = Math.floor(index / this.screenWidth);
+            const x = (index / 4) % this.screenWidth;
+            const y = Math.floor((index / 4) / this.screenWidth);
             switch(prop) {
                 case 0: // red
                 case 1: // green
