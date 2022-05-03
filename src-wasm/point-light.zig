d9c311899bcbd5aea61ca6b06eaf97c39675cf81
Aleks Rutins <aleks@rutins.com>
2022-05-03 10:54:47 -0400

    Try clamping

--- src-wasm/point-light.zig
+++ src-wasm/point-light.zig
@@ -1,7 +1,12 @@
+fn clamp(x: f64, min: f64, max: f64) f64 {
+    if(x < min) return min;
+    if(x > max) return max;
+    return x;
+}
 fn parabola(x: f64, max: u32, radius: u32) f64 {
     const a = @divExact(-@as(i64, radius*radius), max);
     const result = @intToFloat(f64, a)*x*x + @intToFloat(f64, max);
-    return if(result > 0) result else 0;
+    return clamp(result, 0, 255);
 }
 
 fn calculate(x: u32, y: u32, radius: u32, brightness: u32, cx: u32, cy: u32) f64 {
