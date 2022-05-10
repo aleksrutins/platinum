26d874c4dd31687eedfb10595959be4fb75aa0aa
Aleks Rutins <aleks@rutins.com>
2022-05-10 10:50:16 -0400

    Start trying to use Jest

---
+++ __tests__/game-test.ts
@@ -0,0 +1,13 @@
+import { Game, Entity } from "..";
+let game!: Game;
+
+class MockEntity extends Entity {
+}
+
+beforeAll(() => {
+    game = new Game;
+});
+test("successfully adds entities", () => {
+    game.add(new MockEntity("hi"));
+    expect(game.get(MockEntity, "hi")).toBeInstanceOf(MockEntity);
+});
