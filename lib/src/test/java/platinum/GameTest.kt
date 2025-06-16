7fc5e92fa445c24c108cc9696ccbcea5f89d8f82
Aleks Rūtiņš <keeper@farthergate.com>
2025-06-16 16:32:40 -0400

    Fix test

--- lib/src/test/java/platinum/GameTest.kt
+++ lib/src/test/java/platinum/GameTest.kt
@@ -1,15 +1,14 @@
-package platinum;
+package platinum
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-import platinum.ecs.Entity;
+import org.junit.jupiter.api.Test
+import platinum.ecs.Entity
 
-public class GameTest {
-  @Test
-  void canAddEntities() {
-      var game = new Game();
-      var entity = new Entity("helloWorld");
-      game.add(entity);
-      assertTrue(game.get(Entity.class, "helloWorld").isPresent());
-  }
+class GameTest {
+    @Test
+    fun canAddEntities() {
+        val game = Game()
+        val entity = Entity("helloWorld")
+        game.add(entity)
+        assert(game.get<Entity>("helloWorld") != null)
+    }
 }
